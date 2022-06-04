8b32ea3a88c988e054dac49306048a2e5c1c9f61
hoonkim <hoonk9387@gmail.com>
2022-06-04 12:42:00 -0500

    Completed assignment.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe() {
+    /* Callback function event handler. */
+    function focusMessage() {
+        console.log("Good!")
+    }
+    function blurMessage() {
+        console.log("Hey! Eyes on me!")
+    }
+
+    return (
+        /* Add event listeners and pass in event handler 
+           callback function. */
+        <button onFocus={focusMessage} onBlur={blurMessage}>Eyes on me</button>
+    )
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,16 @@
 // Code Keypad Component Here
+import React from "react";
+
+function KeyPad() {
+    /* Callback function event handler. */
+    function enterPassword() {
+        console.log("Entering password...")
+    }
+
+    return (/* Add onChange event listener and pass event handler 
+               callback function. */
+            <input type="password" onChange={enterPassword} />
+        )
+}
+
+export default KeyPad;
